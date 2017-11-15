f5eff016337b97992dbd93e14c8b49918c865984
Brandon Bribiesca <bribiesca.isc93@gmail.com>
2017-11-15 17:08:35 -0600

    Getting parameter injected

--- Services/User.php
+++ Services/User.php
@@ -8,14 +8,26 @@
 
 namespace OsmosUserBundle\Services;
 
+use Symfony\Component\DependencyInjection\Container;
+
 class User extends RequestHandler{
     public static $GET_METHOD = "GET";
     public static $POST_METHOD = "POST";
     public static $PUT_METHOD = "PUT";
-    public static $URL_GETGROUPSBYCOMPANY = "http://outsource.bribiesca.mx/web/app_dev.php/paysheetGroup/{company_id}";
+    public static $OUTSOURCE_PREFIX = "";
+    public static $API_PREFIX = "";
+    public static $PAYSHEET_PREFIX = "";
+
+    public static $URL_GETGROUPSBYCOMPANY = "/paysheetGroup/{company_id}";
+
+    public function __construct($api, $outsource, $paysheet){
+        self::$API_PREFIX = $api;
+        self::$OUTSOURCE_PREFIX = $outsource;
+        self::$PAYSHEET_PREFIX = $paysheet;
+    }
 
     public function getGroupsByCompany($company_id){
-        $url = $this->parseURL(self::$URL_GETGROUPSBYCOMPANY, array("company_id" => $company_id));
+        $url = $this->parseURL(self::$OUTSOURCE_PREFIX . self::$URL_GETGROUPSBYCOMPANY, array("company_id" => $company_id));
         return $this->doRequest(self::$GET_METHOD, $url);
     }
 }
