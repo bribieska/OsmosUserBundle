a2b77f43dd5ad3df6bd419003d6cd579d44de814
Brandon Bribiesca <bribiesca.isc93@gmail.com>
2017-11-15 17:06:04 -0600

    Configuration tree on config.yml added

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -23,6 +23,17 @@ class Configuration implements ConfigurationInterface
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
+        $rootNode
+            ->children()
+                ->arrayNode("projects")
+                    ->isRequired()
+                    ->children()
+                        ->scalarNode("api")->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode("outsource")->isRequired()->cannotBeEmpty()->end()
+                        ->scalarNode("paysheet")->isRequired()->cannotBeEmpty()->end()
+                    ->end()
+                ->end()
+            ->end();
 
         return $treeBuilder;
     }
