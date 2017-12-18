b189d6335a3792655ce9b930fdc62fcc9ef1982f
Fabián <ing.fabiancm@gmail.com>
2017-12-18 16:20:23 -0600

    first commit by fabian

--- Services/User.php
+++ Services/User.php
@@ -10,24 +10,36 @@ namespace OsmosUserBundle\Services;
 
 use Symfony\Component\DependencyInjection\Container;
 
-class User extends RequestHandler{
-    public static $GET_METHOD = "GET";
-    public static $POST_METHOD = "POST";
-    public static $PUT_METHOD = "PUT";
+class User extends RequestHandler
+{
+
     public static $OUTSOURCE_PREFIX = "";
-    public static $API_PREFIX = "";
-    public static $PAYSHEET_PREFIX = "";
+    public static $PAYSHEET_PREFIX  = "";
+    public static $API_PREFIX       = "";
+
+    public static $DELETE_METHOD    = "DELETE";
+    public static $POST_METHOD      = "POST";
+    public static $PUT_METHOD       = "PUT";
+    public static $GET_METHOD       = "GET";
 
     public static $URL_GETGROUPSBYCOMPANY = "/paysheetGroup/{company_id}";
 
-    public function __construct($api, $outsource, $paysheet){
-        self::$API_PREFIX = $api;
+    public static $URL_NEWDOCUMENTTYPE = "/documents/new_file_type/{users}";
+
+
+    public function __construct($api, $outsource, $paysheet)
+    {
+        self::$API_PREFIX       = $api;
         self::$OUTSOURCE_PREFIX = $outsource;
-        self::$PAYSHEET_PREFIX = $paysheet;
+        self::$PAYSHEET_PREFIX  = $paysheet;
     }
 
-    public function getGroupsByCompany($company_id){
+    public function getGroupsByCompany($company_id)
+    {
         $url = $this->parseURL(self::$OUTSOURCE_PREFIX . self::$URL_GETGROUPSBYCOMPANY, array("company_id" => $company_id));
+
         return $this->doRequest(self::$GET_METHOD, $url);
     }
+
+
 }
