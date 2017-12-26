82995e2a19c8834d822055602ebc828348ae02f4
Fabián <ing.fabiancm@gmail.com>
2017-12-26 09:48:43 -0600

    some changes

--- Services/User.php
+++ Services/User.php
@@ -27,28 +27,42 @@ class User extends RequestHandler
 
     /** USER API URL'S */
     // DOCUMENTS
-    public static $URL_NEWDOCUMENTTYPE = "/documents/new_file_type/{users}";
-    public static $URL_DOCUMENTSINVITE = "/documents/user/{user_id}/company/{company_id}/invite";
-    public static $URL_DELETEDOCUMENTS = "/documents/user/{user_id}";
+    public static $URL_CREATEDOCUMENTSDUMMY = "/documents/user/{user_id}/company/{company_id}/dummy";
+    public static $URL_NEWDOCUMENTTYPE      = "/documents/new_file_type/{users}";
+    public static $URL_DOCUMENTSINVITE      = "/documents/user/{user_id}/company/{company_id}/invite";
+    public static $URL_DELETEDOCUMENTS      = "/documents/user/{user_id}";
 
     // USER SALARY
-    public static $URL_SETPAYMENTPERIOD = "/salary/user/{user_id}/pperiod/{payment_period_id}";
-    public static $URL_CREATESALARY     = "/salary/user/{user_id}/company/{company_id}/{data}";
-    public static $URL_DELETESALARY     = "salary/user/{user_id}";
+    public static $URL_CREATESALARYDUMMY = "salary/user/{user_id}/company/{company_id}/dummy";
+    public static $URL_SETPAYMENTPERIOD  = "/salary/user/{user_id}/pperiod/{payment_period_id}";
+    public static $URL_CREATESALARY      = "/salary/user/{user_id}/company/{company_id}/{data}";
+    public static $URL_DELETESALARY      = "salary/user/{user_id}";
 
     // USER INFO
-    public static $URL_CREATEINFO = "/info/user/{user_id}/{data}";
-    public static $URL_DELETEINFO = "info/user/{user_id}";
+    public static $URL_CREATEINFODUMMY = "info/user/{user_id}/dummy";
+    public static $URL_CREATEINFO      = "/info/user/{user_id}/{data}";
+    public static $URL_DELETEINFO      = "info/user/{user_id}";
+    public static $URL_GETINFO         = "/info/user/{user_id}";
 
     // PROCESS
     public static $URL_PROCESS = "/process/user/{user_id}";
 
     // USER ADDRESS
-    public static $URL_CREATEADDRESS = "/address/user/{user_id}/{data}";
+    public static $URL_CREATEADDRESSDUMMY = "/address/user/{user_id}/dummy";
+    public static $URL_CREATEADDRESS      = "/address/user/{user_id}/{data}";
+    public static $URL_DELETEADDRESS      = "/address/user/{user_id}";
 
     // USER BANK
-    public static $URL_CREATEBANK = "/bank/user/{user_id}/{data}";
-    public static $URL_DELETEBANK = "/bank/user/{user_id}";
+    public static $URL_CREATEBANKDUMMY = "/bank/user/{user_id}/dummy";
+    public static $URL_CREATEBANK      = "/bank/user/{user_id}/{data}";
+    public static $URL_DELETEBANK      = "/bank/user/{user_id}";
+
+    // SOCIAL SECURITY
+    public static $URL_DELETESOCIALSECURITY = "socialsecurity/user/{user_id}";
+
+    // IDSE
+
+    public static $URL_DELETEIDSE = "/idse/user/{user_id}";
 
     /**
      * User constructor.
@@ -75,6 +89,17 @@ class User extends RequestHandler
     }
 
     /**
+     * @param $userId
+     * @return string
+     */
+    public function getUserInfoByUserId($userId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_GETINFO, array("user_id" => $userId ));
+
+        return $this->doRequest(self::$GET_METHOD, $url);
+    }
+
+    /**
      * @param $data
      * @param $userId
      * @return string
@@ -82,6 +107,17 @@ class User extends RequestHandler
     public function createUserInfo($userId, $data)
     {
         $url = $this->parseURL(self::$API_USERS . self::$URL_CREATEINFO, array("user_id" => $userId, "data" => $data));
+
+        return $this->doRequest(self::$POST_METHOD, $url);
+    }
+
+    /**
+     * @param $userId
+     * @return string
+     */
+    public function createInfoDummy($userId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_CREATEINFODUMMY, array("user_id" => $userId));
 
         return $this->doRequest(self::$POST_METHOD, $url);
     }
@@ -112,6 +148,18 @@ class User extends RequestHandler
 
     /**
      * @param $userId
+     * @param $companyId
+     * @return string
+     */
+    public function createSalaryDummy($userId, $companyId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_CREATESALARYDUMMY, array("user_id" => $userId, "company_id" => $companyId));
+
+        return $this->doRequest(self::$POST_METHOD, $url);
+    }
+
+    /**
+     * @param $userId
      * @return string
      */
     public function deleteSalaryByUser($userId)
@@ -135,12 +183,45 @@ class User extends RequestHandler
 
     /**
      * @param $userId
+     * @return string
+     */
+    public function createAddressDummy($userId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_CREATEADDRESSDUMMY, array("user_id" => $userId));
+
+        return $this->doRequest(self::$POST_METHOD, $url);
+    }
+
+    /**
+     * @param $userId
+     * @return string
+     */
+    public function deleteAddress($userId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_DELETEADDRESS, array("user_id" => $userId));
+
+        return $this->doRequest(self::$DELETE_METHOD, $url);
+    }
+
+    /**
+     * @param $userId
      * @param $data
      * @return string
      */
     public function createUserBank($userId, $data)
     {
         $url = $this->parseURL(self::$API_USERS . self::$URL_CREATEBANK, array("user_id" => $userId, "data" => $data));
+
+        return $this->doRequest(self::$POST_METHOD, $url);
+    }
+
+    /**
+     * @param $userId
+     * @return string
+     */
+    public function createBankDummy($userId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_CREATEBANKDUMMY, array("user_id" => $userId));
 
         return $this->doRequest(self::$POST_METHOD, $url);
     }
@@ -193,6 +274,18 @@ class User extends RequestHandler
     }
 
     /**
+     * @param $userId
+     * @param $companyId
+     * @return string
+     */
+    public function createDocumentsDummy($userId, $companyId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_CREATEDOCUMENTSDUMMY, array("user_id" => $userId, "company_id" => $companyId));
+
+        return $this->doRequest(self::$POST_METHOD, $url);
+    }
+
+    /**
      * @param $paymentPeriodId
      * @param $userId
      * @return string
@@ -223,6 +316,28 @@ class User extends RequestHandler
     public function deleteProcess($userId)
     {
         $url = $this->parseURL(self::$API_USERS . self::$URL_PROCESS, array("user_id" => $userId));
+
+        return $this->doRequest(self::$DELETE_METHOD, $url);
+    }
+
+    /**
+     * @param $userId
+     * @return string
+     */
+    public function deleteSocialSecurity($userId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_DELETESOCIALSECURITY, array("user_id" => $userId));
+
+        return $this->doRequest(self::$DELETE_METHOD, $url);
+    }
+
+    /**
+     * @param $userId
+     * @return string
+     */
+    public function deleteIDSEbyUserId($userId)
+    {
+        $url = $this->parseURL(self::$API_USERS . self::$URL_DELETEIDSE, array("user_id" => $userId));
 
         return $this->doRequest(self::$DELETE_METHOD, $url);
     }
